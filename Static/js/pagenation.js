5437b7e6086308a855e07c106a06f6d036e65e57
Rinasham <rinashamau@gmail.com>
2022-09-10 22:02:39 +1000

    [add] JSディレクトリにjsファイル追加

---
+++ Static/js/pagenation.js
@@ -0,0 +1,90 @@
+const threads = [
+  { id: 1, name: "RareTECH" },
+  { id: 2, name: "mouseの会" },
+  { id: 3, name: "アニメを語ろう" },
+  { id: 4, name: "クソ漫画はこちら" },
+  { id: 5, name: "RareTECH" },
+  { id: 6, name: "RareTECH" },
+  { id: 7, name: "RareTECH" },
+  { id: 8, name: "RareTECH" },
+  { id: 9, name: "RareTECH" },
+  { id: 10, name: "RareTECH" },
+  { id: 11, name: "RareTECH" },
+  { id: 12, name: "RareTECH" },
+  { id: 13, name: "RareTECH" },
+  { id: 14, name: "RareTECH" },
+  { id: 15, name: "RareTECH" },
+];
+
+const pagination = () => {
+  // 初期設定
+  let page = 1; // 今何ページ目にいるか
+  const STEP = 5; // ステップ数（1ページに表示する項目数）
+  // 全ページ数 threadsリストの総数/ステップ数の余りの有無で場合分け
+  // 余りがある場合はページを１つ余分に追加する
+  const TOTAL =
+    threads.length % STEP == 0
+      ? threads.length / STEP
+      : Math.floor(threads.length / STEP) + 1;
+
+  // <ul class="pagination"></ul> の中身(li)を書き換える
+  const paginationUl = document.querySelector(".pagination");
+  let pageCount = 0;
+  while (pageCount < TOTAL) {
+    let li = document.createElement("li");
+    li.innerText = pageCount + 1;
+    paginationUl.appendChild(li);
+    pageCount++;
+  }
+
+  // <ul class="thread-box"></ul> の中身(li)を書き換える
+  const show = (page, STEP) => {
+    const ul = document.querySelector(".thread-box");
+    // 一度リストを空にする
+    ul.innerHTML = "";
+
+    const first = (page - 1) * STEP + 1;
+    const last = page * STEP;
+    threads.forEach((item, i) => {
+      if (i < first - 1 || i > last - 1) return;
+      let li = document.createElement("li");
+      li.innerText = item.name;
+      ul.appendChild(li);
+    });
+  };
+
+  // pagination内で現在選択されているページの番号に色を付ける
+  const colorPaginationNum = () => {
+    // <ul class="pagination"></ul>内の<li></li>を全て取得し、配列に入れる
+    // ループさせて一度全ての<li></li>から　class="colored"を削除
+    const paginationArr = [...document.querySelectorAll(".pagination li")];
+    paginationArr.forEach((page) => {
+      page.classList.remove("colored");
+    });
+    // 選択されているページに　class="colored"を追加（背景色が変わる）
+    paginationArr[page - 1].classList.add("colored");
+  };
+
+  // 最初に1ページ目を表示
+  show(page, STEP);
+
+  // 前ページ遷移
+  document.getElementById("prev").addEventListener("click", () => {
+    if (page <= 1) return;
+    page = page - 1;
+    show(page, STEP);
+    colorPaginationNum();
+  });
+
+  // 次ページ遷移
+  document.getElementById("next").addEventListener("click", () => {
+    if (page >= threads.length / STEP) return;
+    page = page + 1;
+    show(page, STEP);
+    colorPaginationNum();
+  });
+};
+
+window.onload = () => {
+  pagination();
+};
