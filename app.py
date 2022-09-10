4e520b5c9da6f39316d2a9fb3e2760d8e461b7fc
Rinasham <rinashamau@gmail.com>
2022-09-10 21:34:50 +1000

    [add] signup.htmlを表示

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello world</h1>'
+def test():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
